421d2883999a43331dacf2a67f38c94b31eac8e0
Tyler Weimin Ouyang <ouyang@cs.ucsb.edu>
2016-10-19 09:24:24 -0700

    Swift 2.3 Xcode 8 migration

--- Pods/Target Support Files/Pods-GoldDiggerUITests/Pods-GoldDiggerUITests-frameworks.sh
+++ Pods/Target Support Files/Pods-GoldDiggerUITests/Pods-GoldDiggerUITests-frameworks.sh
@@ -82,14 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Kanna/Kanna.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Kanna/Kanna.framework"
-fi
